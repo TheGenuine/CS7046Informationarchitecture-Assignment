ea2605dcc3e157fa7ae556c91fe3b4a908c79374
Rene Ruck <reneruck@gmail.com>
2013-03-13 15:56:16 +0000

    initial commit

---
+++ src/main/java/ie/tcd/scss/ubicom/informationarchitecture/Main.java
@@ -0,0 +1,13 @@
+package ie.tcd.scss.ubicom.informationarchitecture;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
